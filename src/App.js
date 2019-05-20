e4b1428bccfb2bba01f2681cb7a6af57213dae46
Nikola Perisic <cathode.y2k@gmail.com>
2019-05-20 18:11:31 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+require("dotenv").config();
+class App extends React.Component {
+  state = {
+    error: null,
+    isLoaded: false,
+    items: []
+  };
+  componentDidMount() {
+    const query = `
+{
+  search(query: "javascript", type: REPOSITORY, first: 10) {
+    repositoryCount
+    edges {
+      node {
+        ... on Repository {
+          name
+          descriptionHTML
+          stargazers {
+            totalCount
+          }
+          forks {
+            totalCount
+          }
+          updatedAt
+        }
+      }
+    }
+  }
+}
+`;
+    const variables = {};
+    this.getRepos(query, variables);
+  }
+  getRepos = async (query, variables) => {
+    try {
+      const response = await axios.post(
+        "https://api.github.com/graphql",
+        {
+          query,
+          variables
+        },
+        {
+          headers: {
+            Authorization: `token ${process.env.REACT_APP_TOKEN}`
+          }
+        }
+      );
+      console.log(response.data);
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      this.setState(() => ({
+        isLoaded: true,
+        items: response.data
+      }));
+    } catch (error) {
+      this.setState(() => ({ error }));
+    }
+  };
+  render() {
+    return (
+      <div className="App">
+        <div className="wrapper">
+          <div className="label">Github repository search</div>
+          <label htmlFor="search">
+            <input type="text" name="search" placeholder="search..." />
+          </label>
+          <div className="results">
+            <p>Search results</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
